496595ea2cb4ce0c8ff2d8af4df1b9dd4c1cb5d4
Thomas Schmitt <thomas@linuxmuster.net>
2018-02-12 18:57:11 +0000

    - removed network setup.

--- sbin/linuxmuster-mail.py
+++ sbin/linuxmuster-mail.py
@@ -2,30 +2,26 @@
 #
 # linuxmuster-mail.py
 # thomas@linuxmuster.net
-# 20171122
+# 20180209
 #
 
 import configparser
 import datetime
 import getopt
-import netifaces
 import os
 import sys
 import re
-from IPy import IP
 
 def usage():
     print('Usage: linuxmuster-mail.py [options]')
     print(' [options] may be:')
     print(' -c <file>, --config=<file> : path to ini file with setup values (mandatory with -s)')
-    print(' -n,        --network       : network setup (not with -s)')
     print(' -r,        --reboot        : reboot system after setup')
-    print(' -s,        --setup         : start mailserver setup (needs -c)')
     print(' -h,        --help          : print this help')
 
 # get cli args
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "c:hnrs", ["config=", "help", "network", "reboot", "setup"])
+    opts, args = getopt.getopt(sys.argv[1:], "c:hr", ["config=", "help", "reboot"])
 except getopt.GetoptError as err:
     # print help information and exit:
     print(err) # will print something like "option -a not recognized"
@@ -34,17 +30,11 @@ except getopt.GetoptError as err:
 
 # default values
 reboot = False
-setup = False
-network = False
 config = None
 
 # evaluate options
 for o, a in opts:
-    if o in ("-s", "--setup"):
-        setup = True
-    elif o in ("-n", "--network"):
-        network = True
-    elif o in ("-r", "--reboot"):
+    if o in ("-r", "--reboot"):
         reboot = True
     elif o in ("-c", "--config"):
         if os.path.isfile(a):
@@ -57,24 +47,15 @@ for o, a in opts:
         sys.exit()
     else:
         assert False, "unhandled option"
-if (setup == True and network == True):
-    usage()
-    sys.exit()
-elif (setup == True and config == None):
-    usage()
-    sys.exit()
-elif (setup == False and network == False):
+if (config == None):
     usage()
     sys.exit()
 
-# hostname
-hostname = 'mail'
 # certificate
 mailkey = '/etc/linuxmuster/ssl/mail.key.pem'
 # template dirs
 tpldir = '/usr/share/linuxmuster-mail/templates'
 dockertpldir = tpldir + '/docker'
-networktpldir = tpldir + '/network'
 # docker
 dockerdir = '/srv/docker/linuxmuster-mail/'
 dockerimage = 'tvial/docker-mailserver:latest'
@@ -113,175 +94,6 @@ def writeTextfile(tfile, content, flag):
         print('Failed to write ' + tfile + '!')
         return False
 
-# return detected network interfaces
-def detectedInterfaces():
-    iface_list = netifaces.interfaces()
-    iface_list.remove('lo')
-    iface_count = len(iface_list)
-    if iface_count == 1:
-        iface_default = iface_list[0]
-    else:
-        iface_default = ''
-    return iface_list, iface_default
-
-# return default network interface
-def getDefaultIface():
-    # first try to get a single interface
-    iface_list, iface_default = detectedInterfaces()
-    if iface_default != '':
-        return iface_list, iface_default
-    # second if more than one get it by default route
-    route = "/proc/net/route"
-    with open(route) as f:
-        for line in f.readlines():
-            try:
-                iface, dest, _, flags, _, _, _, _, _, _, _, =  line.strip().split()
-                if dest != '00000000' or not int(flags, 16) & 2:
-                    continue
-                return iface_list, iface
-            except:
-                continue
-    return iface_list, iface_default
-
-# test valid hostname
-def isValidHostname(hostname):
-    try:
-        if (len(hostname) > 63 or hostname[0] == '-' or hostname[-1] == '-'):
-            return False
-        allowed = re.compile(r'[a-z0-9\-]*$', re.IGNORECASE)
-        if allowed.match(hostname):
-            return True
-        else:
-            return False
-    except:
-        return False
-
-# test valid domainname
-def isValidDomainname(domainname):
-    try:
-        for label in domainname.split('.'):
-            if not isValidHostname(label):
-                return False
-        return True
-    except:
-        return False
-
-# test valid ipv4 address
-def isValidHostIpv4(ip):
-    try:
-        ipv4 = IP(ip)
-        if not ipv4.version() == 4:
-            return False
-        ipv4str = IP(ipv4).strNormal(0)
-        if (int(ipv4str.split('.')[0]) == 0 or int(ipv4str.split('.')[3]) == 0):
-            return False
-        for i in ipv4str.split('.'):
-            if int(i) > 254:
-                return False
-        return True
-    except:
-        return False
-
-# network setup
-def networkSetup():
-    print('### linuxmuster-mail: network setup')
-    # get network interfaces
-    iface_list, iface_default = getDefaultIface()
-    while True:
-        iface = input('Enter network interface to use [' + iface_default + ']: ')
-        iface = iface or iface_default
-        if iface in iface_list:
-            break
-        else:
-            print("Invalid entry!")
-    # ip address & netmask
-    default = '10.0.0.3/16'
-    while True:
-        mailipnet = input('Enter ip address with net or bitmask [' + default + ']: ')
-        mailipnet = mailipnet or default
-        try:
-            n = IP(mailipnet, make_net=True)
-            break
-        except ValueError:
-            print("Invalid entry!")
-    mailip = mailipnet.split('/')[0]
-    network = IP(n).strNormal(0)
-    bitmask = IP(n).strNormal(1).split('/')[1]
-    netmask = IP(n).strNormal(2).split('/')[1]
-    broadcast = IP(n).strNormal(3).split('-')[1]
-    # dns server ip
-    o1, o2, o3, o4 = mailip.split('.')
-    default = o1 + '.' + o2 + '.' + o3 + '.1'
-    while True:
-        dnsip = input('Enter dns server ip address [' + default + ']: ')
-        dnsip = dnsip or default
-        if isValidHostIpv4(dnsip):
-            break
-        else:
-            print("Invalid entry!")
-    # gateway ip
-    default = o1 + '.' + o2 + '.' + o3 + '.254'
-    while True:
-        gatewayip = input('Enter gateway ip address [' + default + ']: ')
-        gatewayip = gatewayip or default
-        if isValidHostIpv4(gatewayip):
-            break
-        else:
-            print("Invalid entry!")
-    # domainname
-    default = 'linuxmuster.lan'
-    while True:
-        domainname = input('Enter domainname [' + default + ']: ')
-        domainname = domainname or default
-        if isValidDomainname(domainname):
-            break
-        else:
-            print("Invalid entry!")
-    # patch config templates
-    printr('Writing network configuration ... ')
-    for f in os.listdir(networktpldir):
-        infile = networktpldir + '/' + f
-        # read template
-        rc, content = readTextfile(infile)
-        if not rc:
-            return 1
-        # extract oufile path from first line
-        firstline = re.findall(r'# .*\n', content)[0]
-        outfile = firstline.partition(' ')[2].replace('\n', '')
-        # replace placeholders
-        content = content.replace('@@iface@@', iface)
-        content = content.replace('@@mailip@@', mailip)
-        content = content.replace('@@netmask@@', netmask)
-        content = content.replace('@@network@@', network)
-        content = content.replace('@@broadcast@@', broadcast)
-        content = content.replace('@@gatewayip@@', gatewayip)
-        content = content.replace('@@dnsip@@', dnsip)
-        content = content.replace('@@domainname@@', domainname)
-        # add date string to content
-        content = '# modified by linuxmuster-mail at ' + dtStr() + '\n' + content
-        # write outfile
-        rc = writeTextfile(outfile, content, 'w')
-        if not rc:
-            return 1
-    # hostname
-    if not writeTextfile('/etc/hostname', 'mail', 'w'):
-        return 1
-    print('Success!')
-    # create ssh hostkeys
-    hostkey_prefix = '/etc/ssh/ssh_host_'
-    crypto_list = ['dsa', 'ecdsa', 'ed25519', 'rsa']
-    os.system('rm -f /etc/ssh/*key*')
-    print('Creating ssh host keys ...')
-    for a in crypto_list:
-        printr(' * ' + a + ' host key:')
-        try:
-            os.system('ssh-keygen -t ' + a + ' -f ' + hostkey_prefix + a + '_key -N ""')
-            print(' Success!')
-        except:
-            print(' Failed!')
-            return 1
-    return 0
-
 # mailserver setup
 def mailSetup():
     # read ini file
@@ -293,7 +105,7 @@ def mailSetup():
         print('Certificate file ' + mailkey + ' found!')
     # permissions
     os.system('chgrp docker ' + mailkey)
-    os.system('chmod 750 ' + mailkey)
+    os.system('chmod 640 ' + mailkey)
     try:
         print('Reading setup data ...')
         setup = configparser.ConfigParser(inline_comment_prefixes=('#', ';'))
@@ -352,22 +164,6 @@ def mailSetup():
             outfile = outfile.replace('ldap-groups.cf', 'ldap-aliases.cf')
             if not writeTextfile(outfile, content, 'w'):
                 return 1
-    # only if not on server
-#    if mailip != serverip:
-#        # hostname
-#        rc = writeTextfile('/etc/hostname', 'mail.' + domainname, 'w')
-#        rc, content = readTextfile(networktpldir + '/hosts')
-#        content = content.replace('@@mailip@@', mailip)
-#        content = content.replace('@@domainname@@', domainname)
-#        if not writeTextfile(networktpldir + '/hosts', content, 'w'):
-#            return 1
-#        # interfaces
-#        interfaces = '/etc/network/interfaces.d/linuxmuster'
-#        rc, content = readTextfile(interfaces)
-#        content = re.sub(r'dns-nameservers .*\n', 'dns-nameservers ' + serverip + ' ' + firewallip + '\n', content)
-#        content = re.sub(r'dns-search .*\n', 'dns-search ' + domainname + '\n', content)
-#        if not writeTextfile(interfaces, content, 'w'):
-#            return 1
     # pull docker image
     rc = os.system('docker pull ' + dockerimage)
     if rc != 0:
@@ -381,10 +177,7 @@ def mailSetup():
 ## functions end
 
 
-if network == True:
-    rc = networkSetup()
-elif setup == True:
-    rc = mailSetup()
+rc = mailSetup()
 
 if rc == 0:
     print('\nScript finished successfully!')
